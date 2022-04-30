a9f3806ea307d608aaea2f912f4966a3bb7c4784
ujjwalgupta1 <95112116+ujjwalgupta1@users.noreply.github.com>
2022-04-30 09:24:12 +0530

    Add files via upload

---
+++ extJS.js
@@ -0,0 +1,16 @@
+<script>
+let x=prompt("How old r u",40);
+alert('You are ${x} years old!');
+alert("you are ${x} year old");
+document.getElementById("demo").innerHTML="Hello vishnu";
+x=document.getElementById("demo").innerHTML;
+alert(x);
+alert(typeof(x));
+let a=964896484968494869888888888886;
+alert(typeof a);
+
+function changetext(){
+document.getElementById("d1").innerHTML="Welcome to poornima university"; 
+y=document.getElementById("d1").innerHTML;
+alert(y);}
+</script>
